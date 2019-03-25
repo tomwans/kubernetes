273fa55577a7b8c48ace938ab8ef19c3f0b3b2aa
Tom Wanielista <tomwans@gmail.com>
2019-03-25 16:42:03 -0400

    fix cronjob controller

--- pkg/controller/cronjob/utils.go
+++ pkg/controller/cronjob/utils.go
@@ -200,6 +200,10 @@ func (o byJobStartTime) Less(i, j int) bool {
 		return o[i].Status.StartTime != nil
 	}
 
+	if o[i].Status.StartTime == nil {
+		return false
+	}
+
 	if o[i].Status.StartTime.Equal(o[j].Status.StartTime) {
 		return o[i].Name < o[j].Name
 	}
